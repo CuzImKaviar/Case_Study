156fe1e799920877a3b6c87e909854f0c3fd83a2
Noel Hack <Noel.hack12@gmail.com>
2024-01-11 19:08:58 +0100

    replaceed stuff

--- users.py
+++ users.py
@@ -1,9 +1,5 @@
 class User:
-    def __init__(self, id, name, email) -> None:
+    def __init__(self, id, name) -> None:
         self.name = name
         self.id = id
-        self.email = email
-    
-    def __str__(self):
-        return f"{self.name} ({self.email})"
         
