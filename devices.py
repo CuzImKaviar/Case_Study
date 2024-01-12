9c78fce20dfa8e42d7b82f5e6a0e0029aecff088
Sebastian Bachlechner <sebbachlechner47@gmail.com>
2024-01-12 10:02:29 +0100

    change devices to get no errors

--- devices.py
+++ devices.py
@@ -11,12 +11,14 @@ class Device():
     db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('devices')
 
     # Constructor
-    def __init__(self, device_name : str, managed_by_user_id : str):
+    def __init__(self, device_name : str, managed_by_user_id : str, reservation_start : str, reservation_end : str):
         self.device_name = device_name
         # The user id of the user that manages the device
         # We don't store the user object itself, but only the id (as a key)
         self.managed_by_user_id = managed_by_user_id
         self.is_active = True
+        self.reservation_start = reservation_start
+        self.reservation_end = reservation_end
         
     # String representation of the class
     def __str__(self):
