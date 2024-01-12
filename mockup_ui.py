e197e8921968bdfcd597e6e6f9902ca26233039a
Noel Hack <Noel.hack12@gmail.com>
2024-01-12 11:13:52 +0100

    some changes lol 187

--- mockup_ui.py
+++ mockup_ui.py
@@ -18,26 +18,42 @@ def user_management():
 def device_management():
     st.title("Device Management")
 
-    
+    user_name = st.text_input("Username:")
+    device_name = st.selectbox("Device:", Devices)
+
+
+def reservation_system():
+    st.title("Reservation System")
+
     device_name = st.selectbox("Device:", Devices)
     col1, col2 = st.columns(2)
     device_reservation_start = col1.date_input("Start of reservation:")
     device_reservation_end = col2.date_input("End of reservation:")
     user_email = st.text_input("User email:")
 
-    if st.button("Create/Change device"):
+    if st.button("Create/Change Reservation"):
         if device_reservation_start > device_reservation_end:
             st.error("The start date must not be after the end date.")
         else:
             new_device = Device(device_name=device_name, managed_by_user_id=user_email,reservation_start=device_reservation_start, reservation_end=device_reservation_end)
             st.success(f"Device '{new_device.device_name}' was reservated from '{new_device.reservation_start}' to '{new_device.reservation_end}' by '{user_email}'.")
 
-def reservation_system():
-    st.title("Reservation System")
-    # Implementiere hier die Reservierungssystemfunktionalität...
 
 def maintenance_management():
     st.title("Maintenance Management")
+
+    device_name = st.selectbox("Device:", Devices)
+    col1, col2 = st.columns(2)
+    device_reservation_start = col1.date_input("Start of maintenance interval:")
+    device_reservation_end = col2.date_input("End of maintenance interval:")
+    user_email = st.text_input("User email:")
+
+    if st.button("Create/Change maintenance interval"):
+        if device_reservation_start > device_reservation_end:
+            st.error("The start date must not be after the end date.")
+        else:
+            new_device = Device(device_name=device_name, managed_by_user_id=user_email,reservation_start=device_reservation_start, reservation_end=device_reservation_end)
+            st.success(f"Device '{new_device.device_name}' was reservated from '{new_device.reservation_start}' to '{new_device.reservation_end}' by '{user_email}'.")
     # Implementiere hier die Wartungsmanagementfunktionalität...
 
 selection = st.sidebar.selectbox("Choose an option:", ["User Management", "Device Management", "Reservation System","Maintenance Management"])
