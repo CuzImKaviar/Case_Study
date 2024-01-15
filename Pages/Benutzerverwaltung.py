e37df31b552ce6bbf2a592bb8c4af95b28c4beda
Noel Hack <Noel.hack12@gmail.com>
2024-01-15 10:58:38 +0100

    changes in Benutzerverwaltung

--- Pages/Benutzerverwaltung.py
+++ Pages/Benutzerverwaltung.py
@@ -29,33 +29,33 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
-    options=["Geräte verwalten", "Geräte resvieren", "Geräte anzeigen"],
+    options=["Benutzer verwalten", "Geräte resvieren", "Benutzer anzeigen"],
     icons=["pencil-fill", "clock-history", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
 )
-
+#Benutzer
 
 # --- MANAGE DEVICES ---
-if selected == "Geräte verwalten":
+if selected == "Benutzer verwalten":
 
     manage_selected = option_menu(
         menu_title=None,
-        options=["Geräte hinzufügen", "Geräte bearbeiten", "Geräte entfernen"],
+        options=["Benutzer hinzufügen", "Benutzer bearbeiten", "Benutzer entfernen"],
         icons=["plus-square", "wrench", "dash-square"],  # https://icons.getbootstrap.com/
         orientation="horizontal",
     )
 
-    # --- ADD DEVICES ---
-    if manage_selected == "Geräte hinzufügen":
-        st.header(f"Anlegen eines neuem Gerät")
+    # --- ADD Benutzer ---
+    if manage_selected == "Benutzer hinzufügen":
+        st.header(f"Anlegen eines neuen Benutzers")
         with st.form("entry_form", clear_on_submit=True):
             col1, col2 = st.columns(2)
             col1.selectbox("MCI:", list(map(roman.toRoman,range(1,7))), key="mci")
-            tool_types = ["Office", "EDV", "Labore", "Diverses"]
-            col2.selectbox("Geräte Art:", tool_types, key="type")
-            st.text_input("Gerätename:", max_chars=64, placeholder="Gerätename hier einfügen ...", key="name")
+            tool_types = ["Student", "Mitarbeiter", "Professor", "Diverses"]
+            col2.selectbox(" Tätigkeit am MCI oder so:", tool_types, key="type")
+            st.text_input("E-mail", max_chars=64, placeholder="E-mail hier einfügen ...", key="E-mail")
 
-            "---"
+
 
             with st.expander("Geräteeigenschaften"):
                 st.number_input("Preis:", min_value=0, format="%i", step=10, key="cost")
@@ -68,10 +68,8 @@ if selected == "Geräte verwalten":
                     key="intervals")
                 st.number_input("Kosten pro Wartung:", min_value=0, format="%i", step=1, key="maintenancecost")
                 st.radio("Resavierbarkeit:", ["Resavierbar", "Nicht resavierbar"], horizontal=True, key="resavable")
-            with st.expander("Kommentar"):
-                comment = st.text_area("Kommentarfeld", placeholder="Kommentar hier einfügen ...", label_visibility="collapsed")
+           
 
-            "---"
 
             submitted = st.form_submit_button("Neues Gerät anlegen")
             if submitted:
