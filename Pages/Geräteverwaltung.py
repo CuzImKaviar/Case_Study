62da70c3410e227b2a365d399b9d0acb9f395783
Tobias Stummer <st5390@mci4me.at>
2024-01-15 12:05:53 +0100

    Update Geräteverwaltung.py; changed Date-Format for resavating devices

--- Pages/Geräteverwaltung.py
+++ Pages/Geräteverwaltung.py
@@ -286,7 +286,7 @@ if selected == "Geräte resvieren":
             "Resvierungszeitraum auswählen:",
             (datetime.datetime.now(), datetime.datetime.now()),
             datetime.datetime.now(),
-            format="DD-MM-YYYY",
+            format="DD.MM.YYYY",
         )
 
         "---"
