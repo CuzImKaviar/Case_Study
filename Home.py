995ec0390cd14602e6b8297f1efa0cb2bcb6375d
Tobias Stummer <st5390@mci4me.at>
2024-01-24 17:21:38 +0100

    Add Visuals for Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 # --- Home Page ---
+page_title = "Benutzer- & Geräteverwaltung"
+page_icon = ":house:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
