cd03005511a7b70b278cdcd04f2407ddcc059cc7
Tobias Stummer <st5390@mci4me.at>
2024-01-24 17:29:23 +0100

    Add Buttons to other pages to Home.py & Rename Geräteverwaltung.py to Geraeteverwaltung.py for compadabillity

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 
 # --- Home Page ---
 page_title = "Benutzer- & Geräteverwaltung"
@@ -16,3 +17,35 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# --- NAVIGATE TO OTHER PAGE ---
+def nav_page(page_name, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (page_name, timeout_secs)
+    html(nav_script)
+
+# --- NAVIGATE PAGE ---
+if st.button("Benutzerverwaltung"):
+    nav_page("Benutzerverwaltung")
+if st.button("Geräteverwaltung"):
+    nav_page("Geraeteverwaltung")    
